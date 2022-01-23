88c4b6d4ea4d7661300a24b585ddda44b8612a9f
jmkryzanski <jmkryzanski@gmail.com>
2022-01-22 19:15:44 -0500

    updates

--- learning/forms.py
+++ learning/forms.py
@@ -1,5 +1,10 @@
 from django import forms
-from .models import Lesson, Guide
+from .models import Lesson, Guide, Course
+
+choices = Course.objects.all().values_list('title', 'title')
+choice_list = []
+for item in choices:
+    choice_list.append(item)
 
 class LessonForm(forms.ModelForm):
     class Meta:
@@ -34,7 +39,11 @@ class GuideForm(forms.ModelForm):
                 'class': 'form-control',
                 'placeholder': 'Course'
             }),
-            'language': forms.Textarea(attrs={
+            #'language': forms.Textarea(attrs={
+            #    'class': 'form-control',
+            #    'placeholder': 'Language'
+            #}),
+            'language': forms.Select(choices=choice_list,attrs={
                 'class': 'form-control',
                 'placeholder': 'Language'
             }),
@@ -42,6 +51,10 @@ class GuideForm(forms.ModelForm):
             #    'class': 'form-control',
             #    'placeholder': 'Title'
             #}),
+            'course': forms.Select(choices=choice_list,attrs={
+                'class': 'form-control',
+                'placeholder': 'Content'
+            }),
             'content': forms.Textarea(attrs={
                 'class': 'form-control',
                 'placeholder': 'Content'
