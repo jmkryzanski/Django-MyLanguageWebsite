6e3dd28ce68888348f999ad872daae920493b2a2
jmkryzanski <jmkryzanski@gmail.com>
2022-03-06 16:27:31 -0500

    excel is now working

--- excel/views.py
+++ excel/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseBadRequest, HttpResponse, QueryDict
-#from _compact import JsonResponse
+from django.http import HttpResponseBadRequest
 from django import forms
 import django_excel as excel
-#from .models import Question, Choice
 from learning.models import QuestionAnswer
 
 data = [[1, 2, 3], [4, 5, 6]]
@@ -47,42 +45,3 @@ def import_data(request):
             "header": "Please upload sample-data.xls:",
         },
     )
-
-'''
-def import_data(request):
-    if request.method == "POST":
-        form = UploadFileForm(request.POST, request.FILES)
-
-        def choice_func(row):
-            q = Question.objects.filter(slug=row[0])[0]
-            row[0] = q
-            return row
-
-        if form.is_valid():
-            request.FILES["file"].save_book_to_database(
-                models=[QuestionAnswer, Choice],
-                initializers=[None, choice_func],
-                mapdicts=[
-                    #["question_text", "pub_date", "slug"],
-                    ["question", "answer", "level", "questionKeyword"],
-                    {"Question": "question", "Choice": "choice_text", "Votes": "votes"},
-                    
-                ],
-            )
-            return redirect("handson_view")
-        else:
-            return HttpResponseBadRequest()
-    else:
-        form = UploadFileForm()
-    return render(
-        request,
-        "upload_form.html",
-        {
-            "form": form,
-            "title": "Import excel data into database example",
-            "header": "Please upload sample-data.xls:",
-        },
-    )
-    '''
-
-
